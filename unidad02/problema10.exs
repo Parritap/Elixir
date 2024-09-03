0c3f73450a893eb93ee866b58777f0ceb8a6cef7
Esteban Parra <juane.parrap@uqvirtual.edu.co>
2024-09-02 19:09:21 -0500

    Terminado ejercicio 10}

--- unidad02/problema10.exs
+++ unidad02/problema10.exs
@@ -1,29 +1,17 @@
 defmodule Main do
+
   def main() do
-    calcular_permutaciones_circulares(7)
-    |> IO.puts()
+    "Ingrese la cantidad de clientes"
+    |> Util.ingresar(:entero)
+    |> Util.calcular_permutaciones_circulares()
+    |> generar_mensaje()
+    |> Util.ingresar(:output)
   end
 
-  # método que calcula la permutación circular de un número n.
-  #Método imperativo
-  def calc_permutaciones_circulares(n) do
-    res = n
-
-    if n >= 1 do
-      res * calc_permutaciones_circulares(res - 1)
-    else
-      1
-    end
+  def generar_mensaje(n) do
+    "La cantidad de personas que pueden organizarse es: #{n}"
   end
 
-  #Método funcional
-  def calcular_permutaciones_circulares(n) do
-    (n - 1) |> calcular_factorial
-  end
-
-  #Usando sobrecarga.
-  def calcular_factorial(0), do: 1
-  def calcular_factorial(n), do: n * calcular_factorial(n - 1)
 end
 
 Main.main()
