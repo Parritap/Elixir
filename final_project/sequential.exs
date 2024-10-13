fd3471eb9fedaf294969245a0874b0b731760aa2
Juan Esteban Parra <juane.parrap@uqvirtual.edu.co>
2024-10-13 18:51:07 -0500

    Se obtienen la cantidad de variables y de test_cases de forma exitosa

--- final_project/sequential.exs
+++ final_project/sequential.exs
@@ -1,26 +1,24 @@
 defmodule Sequential do
+  @white_regex ~r/\s+/ # Regex que splitea por caracteres en blanco, cualquier número de estos
+
   def main do
-    num_vars =
-    System.argv()
-    # Gets the first arg which is the file path to text.
-    |> Enum.at(0)
-    |> extract_number_of_vars()
+    file_path = System.argv() |> Enum.at(0)
 
-    num_test_cases = 
+    vars = get_first_line(file_path, &(&1 |> String.split(@white_regex) |> Enum.at(2)))
+    IO.puts(vars)
+
+    num_tests = get_first_line(file_path, &(&1 |> String.split(@white_regex) |> Enum.at(3)))
+     IO.puts(num_tests)
 
   end
 
-  def extract_number_of_vars(file_path) do
-    print_first_non_c_line(file_path)
-    |> String.split(" ")
-    |> Enum.at(2)
-  end
-
-  def print_first_non_c_line(file_path) do
+  @spec get_first_line(String.t(), (String.t() -> any())) :: any()
+  def get_first_line(file_path, function) do
     file_path
     |> File.read!()
     |> String.split("\n")
     |> Enum.find(&(!String.starts_with?(&1, "c")))
+    |> function.()
   end
 end
 
